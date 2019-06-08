603663464a574a143320a187faa85d020df369d3
Andrey Evdokimov <nevdokimof@gmail.com>
2019-06-08 14:54:50 +0300

    added webpack

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,7 +5,7 @@ const buttonsElem: Element = document.querySelector('.timer__break-btn');
 
 const timer: Timer = new Timer(timerElem);
 
-timerElem.addEventListener('click', (event) => {
+timerElem.addEventListener('click', event => {
     const target: Element = <Element>event.target;
     if (timer.isStarted == true ) {
         if (target.matches('.timer__control') && target.classList.contains('timer_cancel')) {
@@ -16,7 +16,7 @@ timerElem.addEventListener('click', (event) => {
     timer.start(25); 
 });
 
-buttonsElem.addEventListener('click', (event) => {
+buttonsElem.addEventListener('click', event => {
     const target: Element = <Element>event.target;
     if (target.matches('.break-btn_type_short')) {
         timer.start(5);
