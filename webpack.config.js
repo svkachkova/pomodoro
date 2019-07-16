b5701de2c651728696ca22e9f7858aa700920e71
SarahH <sv.kachkova@gmail.com>
2019-07-16 20:14:57 +0300

    lint --fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    mode: "production",
+    mode: 'production',
 
     entry: './src/ts/index.ts',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'build/js')
+        path: path.resolve(__dirname, 'build/js'),
     },
 
     devtool: 'source-map',
 
     module: {
-        rules: [{
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-        }]
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
     },
 
     resolve: {
-        extensions: ['.ts']
-    }
+        extensions: ['.ts'],
+    },
 };
