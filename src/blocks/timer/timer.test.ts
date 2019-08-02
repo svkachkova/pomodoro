47ca51421f4ad6ba40e8c8e955d30720791b3f25
Svetlana Kachkova <sv.kachkova@gmail.com>
2019-08-02 20:34:57 +0300

    update tests

--- src/blocks/timer/timer.test.ts
+++ src/blocks/timer/timer.test.ts
@@ -2,50 +2,118 @@ import { expect } from 'chai';
 import 'mocha';
 import Timer from './timer';
 
-describe('timer start(25)', () => {
-    const timerElem: Element = document.createElement('div');
+function createTimer(): Element[] {
+    const timerElement: Element = document.createElement('div');
     const timerTimeElement: Element = document.createElement('div');
     const timerControlElement: Element = document.createElement('div');
 
-    timerElem.classList.add('timer');
+    timerElement.classList.add('timer');
     timerTimeElement.classList.add('timer__time');
     timerControlElement.classList.add('timer__control', 'timer_focus');
 
-    timerElem.appendChild(timerTimeElement).appendChild(timerControlElement);
+    timerElement.appendChild(timerTimeElement).appendChild(timerControlElement);
 
-    const timer: Timer = new Timer(timerElem);
+    return [timerElement, timerTimeElement, timerControlElement];
+}
 
-    timer.start(25);
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-    it('isStarted true', () => {
-        expect(timer.isStarted).to.equal(true);
+describe('timer', () => {
+    describe('timer.start', () => {
+        const [timerElem, timeElem, controlElem]: Element[] = createTimer();
+        const timer: Timer = new Timer(timerElem);
+
+        timer.start(10);
+
+        it('isStarted true', () => {
+            expect(timer.isStarted).to.equal(true);
+        });
+
+        it('control has class "timer_cancel"', () => {
+            expect(controlElem.classList.contains('timer_cancel')).to.equal(true);
+        });
+
+        it('control text is "cancel"', () => {
+            expect(controlElem.innerHTML).to.match(/^cancel$/);
+        });
+
+        it('time is "10:00"', () => {
+            expect(timeElem.innerHTML).to.match(/^10:00$/);
+        });
+
+        it('5 seconds after start time is "09:55"', () => {
+            setTimeout(() => {
+                expect(timeElem.innerHTML).to.match(/^09:55$/);
+            }, 5000);
+        });
     });
 
-    it('control has class "timer_cancel"', () => {
-        expect(timerControlElement.classList.contains('timer_cancel')).to.equal(true);
+    describe('timer.cance()', () => {
+        const [timerElem, timeElem, controlElem]: Element[] = createTimer();
+        const timer: Timer = new Timer(timerElem);
+
+        timer.start(10);
+
+        it('isStarted false', async () => {
+            await timeout(50);
+            timer.cancel();
+
+            expect(timer.isStarted).to.equal(false);
+        });
+
+        it('control has class "timer_focus"', () => {
+            expect(controlElem.classList.contains('timer_focus')).to.equal(true);
+        });
+
+        it('control text is "focus"', () => {
+            expect(controlElem.innerHTML).to.match(/^focus$/);
+        });
+
+        it('time is "00:00"', () => {
+            expect(timeElem.innerHTML).to.match(/^00:00$/);
+        });
     });
 
-    it('control text is "cancel"', () => {
-        expect(timerControlElement.innerHTML).to.match(/^cancel$/);
+    describe('focus', () => {
+        const [timerElem, timeElem, controlElem]: Element[] = createTimer();
+        const timer: Timer = new Timer(timerElem);
+
+        timer.start(25);
+
+        it('25 minutes after start time is "00:00"', () => {
+            setTimeout(() => {
+                expect(timeElem.innerHTML).to.match(/^00:00$/);
+            }, 25 * 60000);
+        });
     });
 
-    it('time is "25:00"', () => {
-        expect(timerTimeElement.innerHTML).to.match(/^25:00$/);
-    });
+    describe('break', () => {
+        describe('short', () => {
+            const [timerElem, timeElem, controlElem]: Element[] = createTimer();
+            const timer: Timer = new Timer(timerElem);
 
-    it('5 seconds after start time is "24:55"', () => {
-        setTimeout(() => {
-            expect(timerTimeElement.innerHTML).to.match(/^24:55$/);
-        }, 5000);
-    });
+            timer.start(5);
 
-    it('document title is "(25m) Focus"', () => {
-        expect(document.title).to.match(/^\(25m\) Focus$/);
-    });
+            it('5 minutes after start time is "00:00"', () => {
+                setTimeout(() => {
+                    expect(timeElem.innerHTML).to.match(/^00:00$/);
+                }, 5 * 60000);
+            });
+        });
 
-    it('25 minutes after start time is "00:00"', () => {
-        setTimeout(() => {
-            expect(timerTimeElement.innerHTML).to.match(/^00:00$/);
-        }, 25 * 60000);
+        describe('long', () => {
+            const [timerElem, timeElem, controlElem]: Element[] = createTimer();
+            const timer: Timer = new Timer(timerElem);
+
+            timer.start(15);
+
+            it('15 minutes after start time is "00:00"', () => {
+                setTimeout(() => {
+                    expect(timeElem.innerHTML).to.match(/^00:00$/);
+                }, 15 * 60000);
+            });
+        });
     });
 });
