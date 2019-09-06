22c49c0233d22c519339821adae4fcacd80a8c65
Svetlana Kachkova <sv.kachkova@gmail.com>
2019-09-06 21:42:55 +0300

    added react component hello

---
+++ src/blocks/hello/hello.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = () => <h1>Hello</h1>; 
+
+// export default Hello;
+
+ReactDOM.render(<Hello />, document.getElementById('hello'));
